4ed26f32dc5485aaf626b3ce77949cff8d1966a5
“YJason-Woo1 git config --global user.name “Jason-Woo <4everjasonwoo@gmail.com>
2021-04-19 17:45:54 -0400

    implement

---
+++ genetic_alg.py
@@ -0,0 +1,79 @@
+import numpy as np
+
+
+class GeneticAlg(object):
+    def __init__(self, idx, candidates, neighbor):
+        self.population_size = 1000
+        self.mutation_rate = 0.01
+        self.max_iter = 1000
+        self.index = idx
+        self.candidates = candidates
+        self.neighbor = neighbor
+
+    def scoring(self, weight):
+        avg_rank = []
+        for i in range(self.population_size):
+            tmp_rank = 0
+            curr_weight = weight[i]
+            score = np.dot(curr_weight.T, self.index)
+            score_sorted_idx = np.argsort(-score)
+            for j, c in enumerate(self.candidates):
+                if c in self.neighbor:
+                    tmp_rank += score_sorted_idx[j]
+            tmp_rank /= len(self.neighbor)
+            avg_rank.append(tmp_rank)
+        return avg_rank
+
+    def keep_fittest(self, rank, population):
+        rank_cpy = rank[:]
+        population_fittest = []
+        for _ in range(self.population_size):
+            curr_min = rank_cpy.index(min(rank_cpy))
+            population_fittest.append(population[curr_min])
+            rank_cpy[curr_min] = 999
+        return population_fittest
+
+    def next_genration_helper(self, num1, num2):
+        num1_bin = '{:08b}'.format(num1)
+        num2_bin = '{:08b}'.format(num2)
+        pos = np.random.randint(1, 8)
+        new_bin = num1_bin[:pos] + num2_bin[pos:]
+        for i in range(8):
+            if np.random.rand() < self.mutation_rate:
+                new_bin = new_bin[:i] + str(1 - int(new_bin[i])) + new_bin[i+1:]
+        return int(new_bin, 2)
+
+    def next_genration(self, population):
+        new_population = []
+        for i in range(self.population_size):
+            for j in range(i + 1, self.population_size):
+                parent1 = population[i]
+                parent2 = population[j]
+                child = []
+                for k in range(4):
+                    child.append(self.next_genration_helper(parent1[k], parent2[k]))
+                new_population.append(child)
+        return np.concatenate((population, new_population))
+
+    def run(self):
+        population = np.random.randint(0, 256, (self.population_size, 4))
+        rank = self.scoring(population)
+
+        best_rank = min(rank)
+        cnt = 0
+        best_rank_history = best_rank
+
+        for _ in range(self.max_iter):
+            population = self.next_genration(population)
+            population = self.keep_fittest(rank, population)
+            rank = self.scoring(population)
+            best_rank = min(rank)
+            if best_rank == best_rank_history:
+                cnt += 1
+                if cnt == 4:
+                    break
+            else:
+                cnt = 0
+                best_rank_history = best_rank
+        best_idx = rank.index(min(rank))
+        return population[best_idx]
--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import numpy as np
+from genetic_alg import *
 
 
 class Model(object):
@@ -10,9 +11,7 @@ class Model(object):
         self.checkin_time_list = []
         self.check_loc_list = []
 
-        self.population_size = 1000
-        self.mutation_rate = 0.01
-        self.max_iter = 1000
+        self.recomm_size = 10
 
     def input_data(self):
         edges_file = '../brightkite/Brightkite_edges.txt'
@@ -126,71 +125,16 @@ class Model(object):
         index_normed = (index - idx_min) / (idx_max - idx_min)
         return index_normed
 
-    def scoring(self, weight, index):
-        avg_rank = []
-        for i in range(self.population_size):
-            tmp_rank = 0
-            curr_weight = weight[i]
-            score = np.dot(curr_weight.T, index)
-            score_sorted_idx = np.argsort(-score)
-            for j, c in enumerate(self.candidates):
-                if c in self.adjacency_list[self.target]:
-                    tmp_rank += score_sorted_idx[j]
-            tmp_rank /= len(self.adjacency_list[self.target])
-            avg_rank.append(tmp_rank)
-        return avg_rank
+    def run(self):
+        index = self.get_idx()
+        genetic = GeneticAlg(index, self.candidates, self.adjacency_list[self.target])
+        weight = genetic.run()
+        score = np.dot(weight.T, index)
+        score_list = score.tolist()
+        recomm_list = []
+        while len(recomm_list) < self.recomm_size:
+            best_idx = score_list.index(min(score_list))
+            if self.candidates[best_idx] not in self.adjacency_list[self.target]:
+                recomm_list.append(self.candidates[best_idx])
+        return recomm_list
 
-    def keep_fittest(self, rank, population):
-        rank_cpy = rank[:]
-        population_fittest = []
-        for _ in range(self.population_size):
-            curr_min = rank_cpy.index(min(rank_cpy))
-            population_fittest.append(population[curr_min])
-            rank_cpy[curr_min] = 999
-        return population_fittest
-
-    def next_genration_helper(self, num1, num2):
-        num1_bin = '{:08b}'.format(num1)
-        num2_bin = '{:08b}'.format(num2)
-        pos = np.random.randint(1, 8)
-        new_bin = num1_bin[:pos] + num2_bin[pos:]
-        for i in range(8):
-            if np.random.rand() < self.mutation_rate:
-                new_bin = new_bin[:i] + str(1 - int(new_bin[i])) + new_bin[i+1:]
-        return int(new_bin, 2)
-
-    def next_genration(self, population):
-        new_population = []
-        for i in range(self.population_size):
-            for j in range(i + 1, self.population_size):
-                parent1 = population[i]
-                parent2 = population[j]
-                child = []
-                for k in range(4):
-                    child.append(self.next_genration_helper(parent1[k], parent2[k]))
-                new_population.append(child)
-        return np.concatenate((population, new_population))
-
-    def genetic_alg(self):
-        population = np.random.randint(0, 256, (self.population_size, 4))
-        idx = self.get_idx()
-        rank = self.scoring(population, idx)
-
-        best_rank = min(rank)
-        cnt = 0
-        best_rank_history = best_rank
-
-        for _ in range(self.max_iter):
-            population = self.next_genration(population)
-            population = self.keep_fittest(rank, population)
-            rank = self.scoring(population, idx)
-            best_rank = min(rank)
-            if best_rank == best_rank_history:
-                cnt += 1
-                if cnt == 4:
-                    break
-            else:
-                cnt = 0
-                best_rank_history = best_rank
-        best_idx = rank.index(min(rank))
-        return population[best_idx]
