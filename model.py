3cddcbc0f810437d2a3bae0a4da4995d56aafa86
“YJason-Woo1 git config --global user.name “Jason-Woo <4everjasonwoo@gmail.com>
2021-04-19 15:44:49 -0400

    implement

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class Model(object):
     def __init__(self):
         self.target = -1
@@ -140,25 +141,48 @@ class Model(object):
         return avg_rank
 
     def keep_fittest(self, rank, population):
-        # TODO
-        return 0
+        rank_cpy = rank[:]
+        population_fittest = []
+        for _ in range(self.population_size):
+            curr_min = rank_cpy.index(min(rank_cpy))
+            population_fittest.append(population[curr_min])
+            rank_cpy[curr_min] = 999
+        return population_fittest
 
-    def next_genration(self):
-        # TODO
-        return 0
+    def next_genration_helper(self, num1, num2):
+        num1_bin = '{:08b}'.format(num1)
+        num2_bin = '{:08b}'.format(num2)
+        pos = np.random.randint(1, 8)
+        new_bin = num1_bin[:pos] + num2_bin[pos:]
+        for i in range(8):
+            if np.random.rand() < self.mutation_rate:
+                new_bin = new_bin[:i] + str(1 - int(new_bin[i])) + new_bin[i+1:]
+        return int(new_bin, 2)
+
+    def next_genration(self, population):
+        new_population = []
+        for i in range(self.population_size):
+            for j in range(i + 1, self.population_size):
+                parent1 = population[i]
+                parent2 = population[j]
+                child = []
+                for k in range(4):
+                    child.append(self.next_genration_helper(parent1[k], parent2[k]))
+                new_population.append(child)
+        return np.concatenate((population, new_population))
 
     def genetic_alg(self):
-        population = np.random.randint(1, 99, (self.population_size, 4))
+        population = np.random.randint(0, 256, (self.population_size, 4))
         idx = self.get_idx()
         rank = self.scoring(population, idx)
 
         best_rank = min(rank)
         cnt = 0
+        best_rank_history = best_rank
 
         for _ in range(self.max_iter):
-            best_rank_history = best_rank
-            self.keep_fittest(rank, population)
-            self.next_genration()
+            population = self.next_genration(population)
+            population = self.keep_fittest(rank, population)
             rank = self.scoring(population, idx)
             best_rank = min(rank)
             if best_rank == best_rank_history:
@@ -167,8 +191,6 @@ class Model(object):
                     break
             else:
                 cnt = 0
-
-
-
-        # TODO
-        return 0
+                best_rank_history = best_rank
+        best_idx = rank.index(min(rank))
+        return population[best_idx]
